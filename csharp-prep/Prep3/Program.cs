87901dc51a61fba977d5c1d3512b79b48fa58337
Esther Tang <zeonytang@gmail.com>
2023-09-20 01:55:57 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,101);
+        Console.WriteLine($"What is the magic number? {randomNumber}");
+        int guess;
+
+        do
+        {
+        Console.Write("What is your guess? ");
+        string response = Console.ReadLine();
+        guess = int.Parse(response);
+        
+        if (guess > randomNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < randomNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else 
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        } while (guess != randomNumber);
     }
 }
