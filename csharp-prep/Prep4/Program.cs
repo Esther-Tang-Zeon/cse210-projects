ceffbe87303e0f3aca064373c9b67c249a13529d
Esther Tang <zeonytang@gmail.com>
2023-10-02 02:11:01 -0600

    Completed prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+        Console.Write("Enter number: ");
+        string response = Console.ReadLine();
+        number = int.Parse(response);
+        numbers.Add(number);
+        } while (number !=0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
