b1788367f2fe60adbee237a7f18afe36de4c24ca
Esther Tang <zeonytang@gmail.com>
2023-10-11 21:50:26 -0600

    Completed encapsulation learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine($"{f1.GetFractionString()}\n{f1.GetDecimalValue()}");
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine($"{f2.GetFractionString()}\n{f2.GetDecimalValue()}");
+
+        Fraction f3 = new Fraction(3,4);
+        Console.WriteLine($"{f3.GetFractionString()}\n{f3.GetDecimalValue()}");
+
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine($"{f4.GetFractionString()}\n{f4.GetDecimalValue()}");
+
+
+        
     }
+
 }
