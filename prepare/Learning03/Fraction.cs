8e6474f8f119b1db0c0a3d857f77b4d9be87068c
Esther Tang <zeonytang@gmail.com>
2023-11-29 10:53:42 -0700

    Redo Learning 03

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,36 +1,42 @@
-using System.IO.Compression;
+using System;
 
 public class Fraction
 {
     private int _top;
     private int _bottom;
 
+    // No argument constructor
     public Fraction()
     {
         _top = 1;
         _bottom = 1;
     }
 
-    public Fraction(int top) 
+    // Constructor with numerator only
+    public Fraction(int top)
     {
         _top = top;
         _bottom = 1;
     }
 
+
+    // Constructor with both numerator and denominator
     public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
     }
 
+    // Method to get the fraction as a string
     public string GetFractionString()
     {
         return $"{_top}/{_bottom}";
-
     }
 
+    // Method to get the decimal value of the fraction 
     public double GetDecimalValue()
     {
         return (double)_top/_bottom;
     }
+
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,19 +6,21 @@ class Program
     {
         Console.WriteLine("Hello Learning03 World!");
 
+        // Using the no-argument constructor
         Fraction f1 = new Fraction();
         Console.WriteLine($"{f1.GetFractionString()}\n{f1.GetDecimalValue()}");
 
+        // Using the constructor with one argument
         Fraction f2 = new Fraction(5);
         Console.WriteLine($"{f2.GetFractionString()}\n{f2.GetDecimalValue()}");
 
-        Fraction f3 = new Fraction(3,4);
+        // Using the constructor with two arguments
+        Fraction f3 = new Fraction(3, 4);
         Console.WriteLine($"{f3.GetFractionString()}\n{f3.GetDecimalValue()}");
 
-        Fraction f4 = new Fraction(1,3);
+        // Using the constructor with two arguments 
+        Fraction f4 = new Fraction(1, 3);
         Console.WriteLine($"{f4.GetFractionString()}\n{f4.GetDecimalValue()}");
-
-
         
     }
 
