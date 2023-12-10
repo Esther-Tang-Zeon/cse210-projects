449d91e86904d13ba307c253d6ed45dd54e11389
Esther Tang <zeonytang@gmail.com>
2023-12-10 03:55:25 -0700

    Learning05 done!

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("blue", 2),
+            new Rectangle("green", 3, 5),
+            new Circle("orange", 6)
+        };
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+
+        
+
+
+
     }
 }
